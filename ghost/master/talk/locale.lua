725b37b2128eb4be0a9f6ce1c8fd0cfcfbab5daf
Tatakinov <tatakinov@gmail.com>
2022-05-31 22:37:59 +0900

    i18nのサンプル

---
+++ ghost/master/talk/locale.lua
@@ -0,0 +1,16 @@
+return {
+-- locale.lua
+-- sample.lua
+  {
+    i18n = true,
+    id  = "goodbye",
+    content = {
+        Japanese  = "\\0またね！\\w9\\w9\\-",
+        English  = "\\0See ya!\\w9\\w9\\-",
+    },
+  },
+-- system/baseware.lua
+-- system/notify.lua
+-- system/debug.lua
+-- _util.lua
+}
